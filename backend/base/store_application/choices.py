2cc8a09d1df17ff5f6eafdf24807f4ac12673e79
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-11-06 10:56:54 +0800

    init: api_vies

---
+++ backend/base/store_application/choices.py
@@ -0,0 +1,13 @@
+STAFF_LEVEL_CHOICES = (
+    ("SELLER", "Seller"),
+    ("BUYER", "Buyer"),
+    ("GUEST", "Guest"),
+    ("ADMIN", "Admin"),
+)
+
+TRANSACTION_STATUS_CHOICES = (
+    ("FAILED", "Failed"),
+    ("PENDING", "Pending"),
+    ("PROCESSING", "Processing"),
+    ("SUCCESSFUL", "Successful"),
+)
