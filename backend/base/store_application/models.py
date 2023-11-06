83d3f4432cc04e8c64c6a45562125a20fbafd41e
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-11-06 10:55:49 +0800

    create: user_models

--- backend/base/store_application/models.py
+++ backend/base/store_application/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .choices import STAFF_LEVEL_CHOICES, TRANSACTION_STATUS_CHOICES
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    staff_level = models.CharField(
+        choices=STAFF_LEVEL_CHOICES, max_length=50, blank=True, null=True
+    )
