b0447f6d7f191cb7bf735fa1a21839bc23734648
ArlieT <115807342+ArlieT@users.noreply.github.com>
2023-11-06 16:43:36 +0800

    reset

--- frontend/lib/form/form-schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const formSchema = z.object({
-  email: z.string().min(2, { message: "min is 2" }).max(50),
-  password: z.string().min(6).max(50),
-});
-
-export type formType = z.infer<typeof formSchema>;
