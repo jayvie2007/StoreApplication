58a76ff30a38413e5b143f95567c1ab4b4732849
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-11-06 17:48:57 +0800

    setup: gitignore

--- backend/base/base/settings.py
+++ backend/base/base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     'store_application',
 ]
@@ -118,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'store_application.User'
